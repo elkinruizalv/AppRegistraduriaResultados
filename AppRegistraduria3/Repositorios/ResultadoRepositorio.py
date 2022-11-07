2460faa570c60a983f5cb8c4dfc86d1c202cf018
elkinruizalv <elferrual@gmail.com>
2022-11-07 13:46:01 -0500

    Elkin Ruiz 2022-11-07

---
+++ AppRegistraduria3/Repositorios/ResultadoRepositorio.py
@@ -0,0 +1,17 @@
+from Repositorios.InterfazRepositorio import InterfazRepositorio
+from Modelos.Resultado import Resultado
+from bson import ObjectId
+
+class ResultadoRepositorio(InterfazRepositorio[Resultado]):
+
+    #Devuelve los candidatos votados en la mesa
+    def getListadoCandidatosInscritosMesa(self, id_mesa):
+        theQuery = {"mesa.$id": ObjectId(id_mesa)}
+        return self.query(theQuery)
+
+    #Función que revisa el canadidato votado en cada mesa
+    def getListadoMesasCandidatoInscrito(self, id_candidato):
+        theQuery = {"candidato.$id": ObjectId(id_candidato)}
+        return self.query(theQuery)
+    
+
